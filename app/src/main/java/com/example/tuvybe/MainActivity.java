c5ad8adf15e1958a337e9816075a6bbe375f15d0
Sheila Sharon <devsheilawambui@gmail.com>
2021-06-08 11:34:14 +0300

    edit layout

--- app/src/main/java/com/example/tuvybe/MainActivity.java
+++ app/src/main/java/com/example/tuvybe/MainActivity.java
@@ -4,13 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.spalshImage)
+    ImageView mSplashImage;
+    @BindView(R.id.appTitle)TextView mAppTitle;
+    @BindView(R.id.appAbout)TextView mAppAbout;
+
+    @BindView(R.id.splashButton)
+    Button mSplashButton;
+
+    //animation variables
+    Animation topAnim,bottomAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+        //define animation context
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim= AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //hooks
+        mSplashImage.setAnimation(topAnim);
+        mAppTitle.setAnimation(topAnim);
+        mAppAbout.setAnimation(bottomAnim);
+        mSplashButton.setAnimation(bottomAnim);
+
+
     }
 }
