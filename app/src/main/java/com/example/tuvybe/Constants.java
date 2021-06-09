f9c8582e02817124f470c24351ac061645cdd834
Sheila Sharon <devsheilawambui@gmail.com>
2021-06-09 12:02:39 +0300

    add constants class and api credentials

---
+++ app/src/main/java/com/example/tuvybe/Constants.java
@@ -0,0 +1,6 @@
+package com.example.tuvybe;
+
+public class Constants {
+    public static  final String GIANT_BOMB_BASE_URL="https://www.eventbriteapi.com/v3/";
+    public static final String GIANT_BOMB_KEY = BuildConfig.EVENT_BRITE_API_KEY;
+}
---
+++ app/src/main/java/com/example/tuvybe/network/EventsAPI.java
@@ -0,0 +1,4 @@
+package com.example.tuvybe.network;
+
+public interface EventsAPI {
+}
